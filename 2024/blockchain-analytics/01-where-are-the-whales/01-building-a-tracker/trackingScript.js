77638e36672826729498a8727f96f092dadc3c89
Akshata Mohanty <akshatamohanty@gmail.com>
2024-01-31 07:48:42 +0800

    ba24 - add better tracking script

--- 2024/blockchain-analytics/01-where-are-the-whales/01-building-a-tracker/trackingScript.js
+++ 2024/blockchain-analytics/01-where-are-the-whales/01-building-a-tracker/trackingScript.js
@@ -1,24 +1,30 @@
-const ethers = require("ethers");
-const walletData = require("../00-blockchain-exploration/sorted-wallets.json");
+require("dotenv").config();
+// It looks like Cloudflare doesn't support the RPC method eth_newPendingTransactionFilter, One workaround is to use your own local node instead of a hosted one.
+// newPendingTransactionFilter
+// https://ts.cloudflare.community/web3/ethereum-gateway/reference/supported-api-methods/
+const DEFAULT_URL = "https://cloudflare-eth.com";
+const nodeUrl = process.env.NODE_URL || DEFAULT_URL;
+const { ethers } = require("ethers");
+const walletData = require("../00-blockchain-exploration/sortedWallets.json");
+const {
+  getTransactionDetails,
+} = require("../02-blockchain-indexing/getTransactions");
 const genesisWallets = walletData.slice(0, 1000).map((t) => t.wallet);
 
 async function main() {
   // Connect to the Ethereum node
-  const provider = new ethers.providers.JsonRpcProvider(
-    "https://cloudflare-eth.com"
-  );
-
+  const provider = new ethers.JsonRpcProvider(nodeUrl);
   // Subscribe to new incoming transactions
   provider.on("pending", async (tx) => {
-    const receipt = await provider.getTransactionReceipt(tx);
-    console.log(`Received a transaction: ${tx}`);
-    console.log(`Block Number: ${receipt.blockNumber}`);
-    console.log(`Transaction Hash: ${receipt.transactionHash}`);
-
-    if (receipt && genesisWallets.indexOf(receipt.to)) {
-      // TODO: Call a google cloud function or publish to pub/sub topic.
+    const txnDetails = await getTransactionDetails(tx, DEFAULT_URL);
+    if (genesisWallets.indexOf(txnDetails?.from) > -1) {
+      console.log(`Txn from genesis wallet holder: ${txnDetails?.from}`);
+      console.log("Details", txnDetails.value);
+      // TODO: Call a cloud function.
+    } else {
+      console.log("Skipping txn: (tx, from)", tx, txnDetails?.from);
     }
   });
 }
 
-main(walletAddress);
+main();
